59695a1c769a9eab9226cc9f2c1a4175a3b1d855
kurotaku <kuroiwa.taku@gmail.com>
2021-05-12 13:14:27 +0900

    temp

--- app/decorators/company_decorator.rb
+++ app/decorators/company_decorator.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 class CompanyDecorator < ApplicationDecorator
+  include ActionView::Helpers::AssetUrlHelper
   delegate_all
 
   def thumbnail
-    image? ? image.thumb.url : "company_default"
+    image? ? image.thumb.url :  helpers.asset_path("company_default.png")
   end
 end
