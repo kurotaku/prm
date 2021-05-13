3ca6b2f0ab91d7eae1f783f2d71715fea58eef02
kurotaku <kuroiwa.taku@gmail.com>
2021-05-13 10:08:08 +0900

    temp

---
+++ config/initializers/aws.rb
@@ -0,0 +1,10 @@
+# creds = Aws::Credentials.new(
+#   ENV['ses_access_key_id'],
+#   ENV['SES_SECRET_ACCESS_KEY']
+# )
+
+# Aws::Rails.add_action_mailer_delivery_method(
+#   :ses,
+#   credentials: creds,
+#   region: 'ap-northeast-1'
+# )
