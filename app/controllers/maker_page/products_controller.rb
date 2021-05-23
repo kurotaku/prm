89e2efec17f7df91ecd8f57d113b5a3e1488fb08
shiorina <toraiwa.shiori@gmail.com>
2021-05-23 11:59:39 +0900

    メーカーページの商品new,create実装

--- app/controllers/maker_page/products_controller.rb
+++ app/controllers/maker_page/products_controller.rb
@@ -8,6 +8,14 @@ class MakerPage::ProductsController < MakerPage::MakerPageController
   end
 
   def create
+     @product = @current_maker_group.products.build(product_params)
+    if @product.save
+      flash[:success] = t('products.create.success')
+      redirect_to maker_page_products_path
+    else
+      flash.now[:error] = t('products.create.error')
+      render :new
+    end
   end
 
   def show
@@ -26,6 +34,6 @@ class MakerPage::ProductsController < MakerPage::MakerPageController
   private
 
     def product_params
-      params.require(:product).permit(:maker_group, :code, :name, :image)
+      params.require(:product).permit(:maker_group, :code, :name, :image, :agency_price, :agency_incentive, :agency_description, :introduction_price, :introduction_incentive, :introduction_description, :wholesale_price, :wholesale_description)
     end   
 end
