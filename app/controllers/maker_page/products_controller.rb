68e0a2173df9426e07d98a1b520747d36515ad1d
shiorina <toraiwa.shiori@gmail.com>
2021-05-24 00:00:43 +0900

    メーカーメニュー 商品 edit update

--- app/controllers/maker_page/products_controller.rb
+++ app/controllers/maker_page/products_controller.rb
@@ -1,4 +1,6 @@
 class MakerPage::ProductsController < MakerPage::MakerPageController
+  before_action :set_product, only: %i[show edit update destroy]
+
   def index
     @products = @current_maker_group.products.all.order(created_at: 'DESC').page(params[:page]).per(10).decorate
   end
@@ -18,20 +20,29 @@ class MakerPage::ProductsController < MakerPage::MakerPageController
     end
   end
 
-  def show
-    @product = Product.find_by(uid: params[:uid]).decorate
-  end
+  def show; end
+    
 
-  def edit
-  end
+  def edit; end
 
   def update
+    if @product.update(product_params)
+      flash[:success] = t('products.update.success')
+      redirect_to maker_page_product_path(uid: @product.uid)
+    else
+      flash.now[:danger] = t('products.update.error')
+      render :edit
+    end
   end
 
   def destroy
   end
 
   private
+
+    def set_product
+      @product = Product.find_by(uid: params[:uid]).decorate
+    end
 
     def product_params
       params.require(:product).permit(:maker_group, :code, :name, :image, :agency_price, :agency_incentive, :agency_description, :introduction_price, :introduction_incentive, :introduction_description, :wholesale_price, :wholesale_description)
